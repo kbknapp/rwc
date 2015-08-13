4b02044b16d34d099e1edd08bc844f180684f2d5
Kevin K <kbknapp@gmail.com>
2015-08-13 00:53:27 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# rwc
+a near clone of the Unix wc utility as a demo case for the rusty-utility series
+
+## LICENSE
+rwc is and associated Rusty Utility series is released under the terms of either the MIT or Apache 2.0 license. See the LICENSE-MIT or LICENSE-APACHE for more details.
---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use std::fs::File;
+use std::io::Read;
+
+static IN_FILE: &'static str = "test_input.txt";
+
+fn main() {
+    // Open and read the file
+    let mut file = File::open(IN_FILE).ok().expect("failed to open file");
+    let mut s = String::new();
+    file.read_to_string(&mut s).expect("failed to read file");
+   
+    // Count the lines (types for clarity)
+    let lines: u64 = s.lines()
+                      .map(|_| 1)
+                      .fold(0, |acc, n| acc + n);
+    
+    // Display the result
+    println!("{} {}", lines, IN_FILE);
+}
