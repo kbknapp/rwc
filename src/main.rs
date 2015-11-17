68485a13589b4f2ce446b733de8d7fdd8bb0adfe
Kevin K <kbknapp@gmail.com>
2015-11-17 00:10:33 -0500

    chore: updates

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,14 @@ static IN_FILE: &'static str = "test_input.txt";
 
 fn main() {
     // Open and read the file
-    let mut file = File::open(IN_FILE).ok().expect("failed to open file");
+    let mut file = File::open(IN_FILE).expect("failed to open file");
     let mut s = String::new();
     file.read_to_string(&mut s).expect("failed to read file");
    
-    // Count the lines (types for clarity)
-    let lines: u64 = s.lines()
-                      .map(|_| 1)
-                      .fold(0, |acc, n| acc + n);
-    
+    // Count the lines
+    let lines = s.lines()
+                 .count(); // usize
+
     // Display the result
     println!("{} {}", lines, IN_FILE);
 }
